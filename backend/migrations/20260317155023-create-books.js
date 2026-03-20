0927f97d83a49921a455bbb49e251e34a2018dad
Edson Oliveira <edso06@gmail.com>
2026-03-20 00:09:32 -0300

    Remove path-scurry package and its ESM build files

--- backend/migrations/20260317155023-create-books.js
+++ backend/migrations/20260317155023-create-books.js
@@ -3,20 +3,49 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Books', {
+    await queryInterface.createTable('books', {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
       },
-      title: {
+      name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      author: {
+      isbn: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
+      },
+      publishing_company: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      writer: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      genre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      n_pages: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      year_publication: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      img: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      synopsis: {
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
@@ -30,6 +59,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Books');
+    await queryInterface.dropTable('books');
   },
 };
